f96cbf39ed8de1b6dfcff8390e3e93316be34414
Darek Litwiniec <lidarek@gmail.com>
2024-01-01 10:34:15 +0100

    bociek

--- src/components/hero_section/HeroOver.jsx
+++ src/components/hero_section/HeroOver.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+
 import styles from './Hero.css'
 
 const HeroOver = () => {
   return (
     <>
       <div className="mag">
-        <h1>May the magic be with you</h1>
+        <h1 >May the magic be with you</h1>
+        
       </div>
       <div className="cont"></div>
     </>
